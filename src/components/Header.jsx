c521c4d03000d84ef34fa5c3c19d76c58b5836b9
ADepTUAKh <fuchkoaa@gmail.com>
2023-05-15 13:39:18 +0200

    https://github.com/it-kamasutra/react-way-of-samurai.git

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+ return (
+  <header className="header">
+   <img src="./favicon.ico" alt="logo" />
+  </header>
+ );
+};
+
+export default Header;
