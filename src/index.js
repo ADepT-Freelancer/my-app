682c218df549a71b9ce45bed1e1bb46695a3de52
ADepTUAKh <fuchkoaa@gmail.com>
2023-05-30 14:48:02 +0200

    ошибка в ИНДЕКСЕ ДЖС 30,05,23

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./redux/redux-store";
 import { Provider } from "react-redux";
 
-
-ReactDOM.render(
+debugger
+ReactDOM.render (
   <BrowserRouter>
     <Provider value={store}>
       <App />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+    </Provider >
+  </BrowserRouter>,  document.getElementById(  "root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// reportWebVitals();
+ServiceWorker.unregister();
